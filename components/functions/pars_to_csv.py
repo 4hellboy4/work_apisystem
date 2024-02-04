680be6047e3ec784a3dd77cf34bad3d1c836fb40
4hellboy4 <Fred19811@yandex.ru>
2024-02-04 12:50:38 +0300

    done parsing from 3 sections and saving to csv

--- components/functions/pars_to_csv.py
+++ components/functions/pars_to_csv.py
@@ -10,7 +10,7 @@ def upload_to_csv(json_list: JsonList) -> None:
     try:
         df_cleaned = pd.DataFrame(json_list.items)
         df_cleaned.index += 1
-        df_cleaned.to_csv('data/csv_files/final6.csv', quoting=csv.QUOTE_ALL, float_format="%.0f", encoding='utf-8',
+        df_cleaned.to_csv('data/csv_files/final2.csv', quoting=csv.QUOTE_ALL, float_format="%.0f", encoding='utf-8',
                           index=True)
     except Exception as e:
         print(f"Error: {e}")
