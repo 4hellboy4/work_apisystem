7da57eca5ef0b84b9f2cab218c5df4f1bb1bc466
4hellboy4 <Fred19811@yandex.ru>
2024-02-04 10:08:25 +0300

    done with draft branch

--- components/functions/pars_to_csv.py
+++ components/functions/pars_to_csv.py
@@ -10,6 +10,7 @@ def upload_to_csv(json_list: JsonList) -> None:
     try:
         df_cleaned = pd.DataFrame(json_list.items)
         df_cleaned.index += 1
-        df_cleaned.to_csv('data/csv_files/final5.csv', quoting=csv.QUOTE_ALL, encoding='utf-8', index=True)
+        df_cleaned.to_csv('data/csv_files/final6.csv', quoting=csv.QUOTE_ALL, float_format="%.0f", encoding='utf-8',
+                          index=True)
     except Exception as e:
         print(f"Error: {e}")
--- components/functions/process_link.py
+++ components/functions/process_link.py
@@ -3,7 +3,7 @@ import json
 from components.functions.request_url import get_json_from_url
 
 
-def process_link(model_id: str, t_dict: dict, count: int) -> json:
+def process_link(model_id: str, t_dict: dict) -> json:
     api_key: str = '0c8bd89c2e517164204ceb1dfee65289a659b4bcffee'
     link_product: str = f'http://market.apisystem.name/models/{model_id}/specification?&format=json&api_key={api_key}'
     link_offers: str = f'http://market.apisystem.name/models/{model_id}/offers?&format=json&api_key={api_key}'
@@ -14,9 +14,5 @@ def process_link(model_id: str, t_dict: dict, count: int) -> json:
 
     t_dict.update(prod_characteristics)
     t_dict.update(prod_offers)
-
-    with open(f'data/draft_files/draft_{count}.json', 'w', encoding='utf-8') as file:
-        json.dump(t_dict, file, ensure_ascii=False)
-        file.close()
 
     return t_dict
